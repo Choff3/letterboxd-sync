daefb4f49857bcf8ced234a064126c4987933697
Choff3 <teddyhoff@gmail.com>
2023-08-05 19:07:39 -0400

    Adding logic complete. pyarr busted

--- radarr_monitor.py
+++ radarr_monitor.py
@@ -15,6 +15,12 @@ def radarr_monitor_add(missing_films):
 
     radarr = RadarrAPI(host_url, api_key)
 
-    # Get and print TV Shows
+    quality_profile_num = 0
+    quality_profile = radarr.get_quality_profile()[quality_profile_num]['id']
 
-    # print(radarr.get_movie())
+    root_dir_num = 0
+    root_dir = radarr.get_root_folder()[root_dir_num]['path']
+
+    for tmdbid in missing_films:
+        film = radarr.lookup_movie_by_tmdb_id(tmdbid)
+        radarr.add_movie(film,root_dir,quality_profile)
