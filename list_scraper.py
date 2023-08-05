e3a63178a848d1d1cfcb9b98754da89ca02f4f10
Choff3 <teddyhoff@gmail.com>
2023-08-05 17:38:08 -0400

    Scraper now grabs tmdb id

--- list_scraper.py
+++ list_scraper.py
@@ -21,24 +21,17 @@ def scrape_list(list_link):
         films = table.find_all('li')
         
         for film in tqdm(films):
-            
-            panel = film.find('div').find('img')
-            title = panel['alt']
 
             film_card = film.find('div').get('data-target-link')
             film_page = 'https://letterboxd.com/' + film_card
             filmget = requests.get(film_page)
             film_soup = BeautifulSoup(filmget.content, 'html.parser')
 
-            director = film_soup.find('meta', attrs={'name':'twitter:data1'}).attrs['content']
-
             # TODO: Parse the return of below to get the TMDB ID. Then use that to add movies to Plex watchlist and Radarr.
-            tmdb_link = film_soup.find_all("a", href=re.compile(r"https://www.themoviedb.org/movie/"))
-            for tmdb in tmdb_link:
-                print(tmdb['href'])
-
-            parsed_film = (title,director)
-            watchlist.append(parsed_film)
+            tmdb_urls = film_soup.find_all("a", href=re.compile(r"https://www.themoviedb.org/movie/"))
+            for tmdb_url in tmdb_urls:
+                tmdb_id = re.search(r'^https:\/\/www\.themoviedb\.org\/movie\/([0-9]+)\/',tmdb_url['href'])
+                watchlist.append(tmdb_id.group(1))
 
         next = soup.find('a', class_='next')
         if next is None:
@@ -46,4 +39,5 @@ def scrape_list(list_link):
         else:
             list_link = 'https://letterboxd.com/' + next['href']
             
+    print(watchlist)
     return watchlist
