0b936c9a7ec21338b3c961503aefb7bbe5db49a0
Choff3 <teddyhoff@gmail.com>
2023-08-06 17:21:06 -0400

    Change from pyarr to requests

--- radarr_monitor.py
+++ radarr_monitor.py
@@ -1,26 +1,19 @@
-from pyarr import RadarrAPI
+import requests
 import os
 
 def radarr_monitor_add(missing_films):
 
-    # Set Host URL and API-Key
-
     host_url = os.getenv('RADARR_HOST')
-
-    # You can find your API key in Settings > General.
-
     api_key = os.getenv('RADARR_TOKEN')
 
-    # Instantiate Radarr Object
-
-    radarr = RadarrAPI(host_url, api_key)
-
-    quality_profile_num = 0
-    quality_profile = radarr.get_quality_profile()[quality_profile_num]['id']
-
-    root_dir_num = 0
-    root_dir = radarr.get_root_folder()[root_dir_num]['path']
+    root_dir = requests.get(host_url+'/api/v3/rootfolder', params={"apikey": api_key}).json()[0]['path']
 
     for tmdbid in missing_films:
-        film = radarr.lookup_movie_by_tmdb_id(tmdbid)
-        radarr.add_movie(film,root_dir,quality_profile)
+        movie = requests.get(host_url+'/api/v3/movie/lookup/tmdb?tmdbId='+tmdbid, params={"apikey": api_key}).json()
+
+        movie["QualityProfileId"] = 1
+        movie["rootFolderPath"] = root_dir
+        movie["monitored"] = True
+        movie["searchOnAdd"] = True
+
+        requests.post(host_url+'/api/v3/movie', params={"apikey": api_key}, json=movie)
