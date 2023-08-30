4d0af387407c1079d63966f433f8cc1781a0959b
Teddy Hoff <teddy.hoff@gtri.gatech.edu>
2023-08-30 14:07:54 -0400

    Plex movies removal

--- plex_watchlist.py
+++ plex_watchlist.py
@@ -14,7 +14,17 @@ def plex_watchlist_add(letterboxdFilms,plex_host,plex_token):
         try:
             plex_film = server.library.section('Movies').getGuid('tmdb://'+film)
             plexAccount.addToWatchlist(plex_film)
+            print("Added "+plex_film.guids[1].id+" to Plex watchlist")
         except:
             missing_films.append(film)
+
+    plexWatchlist = plexAccount.watchlist()
+
+    for film in plexWatchlist:
+        tmdbid = film.guids[1].id.split('tmdb://',1)[1]
+        if tmdbid not in letterboxdFilms:
+            plex_film = server.library.section('Movies').getGuid('tmdb://'+tmdbid)
+            plexAccount.removeFromWatchlist(plex_film)
+            print("Removed tmdb://"+tmdbid+" from Plex watchlist")
 
     return missing_films
