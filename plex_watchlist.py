cfa18ffb4bb945083e7780dc4c3809ba37d201a6
choff6 <choff6@gatech.edu>
2025-09-23 17:14:06 -0400

    simplified thanks to screeny05

--- plex_watchlist.py
+++ plex_watchlist.py
@@ -2,35 +2,36 @@ from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
 import os
 
-def plex_watchlist_add(letterboxdFilms,plex_host,plex_token):
-
-    missing_films = []
+def plex_watchlist_sync(letterboxdFilms,plex_host,plex_token):
 
     server = PlexServer(plex_host, plex_token)
 
     plexAccount = MyPlexAccount(token=plex_token)
 
+    plexWatchlist = plexAccount.watchlist()
+    plexImdbs = []
+    for film in plexWatchlist:
+        imdbid = film.guids[0].id.split('imdb://',1)[1]
+        plexImdbs.append(imdbid)
+
     for film in letterboxdFilms:
         try:
-            plex_film = server.library.section('Movies').getGuid('tmdb://'+film)
+            plex_film = server.library.section('Movies').getGuid('imdb://'+film["imdb_id"])
+            print("Found "+film["title"]+" on Plex server")
         except:
-            print("tmdb://"+film+" not available in Plex")
-            missing_films.append(film)
+            print(film["title"]+" could not be found on Plex server")
             continue
-        try:
+
+        if film["imdb_id"] in plexImdbs:
+            print(film["title"]+" is already on Plex watchlist")
+        else:
             plexAccount.addToWatchlist(plex_film)
-            print("Added "+plex_film.guids[1].id+" to Plex watchlist")
-        except:
-            print("tmdb://"+film+" already on watchlist")
-            continue
+            print("Added "+film["title"]+" to Plex watchlist")
 
-    plexWatchlist = plexAccount.watchlist()
+        # Remove from plexImdbs so we are left with films to be removed from Plex watchlist.
+        plexImdbs.remove(film["imdb_id"])
 
-    for film in plexWatchlist:
-        tmdbid = film.guids[1].id.split('tmdb://',1)[1]
-        if tmdbid not in letterboxdFilms:
-            plex_film = server.library.section('Movies').getGuid('tmdb://'+tmdbid)
-            plexAccount.removeFromWatchlist(plex_film)
-            print("Removed tmdb://"+tmdbid+" from Plex watchlist")
-
-    return missing_films
+    for imdbid in plexImdbs:
+        plex_film = server.library.section('Movies').getGuid('imdb://'+imdbid)
+        plexAccount.removeFromWatchlist(plex_film)
+        print("Removed "+plex_film+" from Plex watchlist")
