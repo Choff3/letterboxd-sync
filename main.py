03281bf02bbdab1a37a6223d691ae4d317c3b774
choff6 <choff6@gatech.edu>
2025-09-25 07:46:01 -0400

    unified script

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from plex_watchlist import *
+from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
 import os
 import requests
 
-def main():
+def plex_watchlist_sync(plex_host,plex_token):
 
     # Scrape letterbox watchlist
     try:
@@ -13,12 +14,50 @@ def main():
         print("Successfully grabbed "+watchlist_url)
     except:
         print("Failed to connect to Letterboxd")
+        return
+
+    server = PlexServer(plex_host, plex_token)
+
+    plexAccount = MyPlexAccount(token=plex_token)
+
+    plexWatchlist = plexAccount.watchlist()
+    plexImdbs = []
+    for film in plexWatchlist:
+        imdbid = film.guids[0].id.split('imdb://',1)[1]
+        plexImdbs.append(imdbid)
+
+    for film in watchlist:
+        try:
+            plex_film = server.library.section('Movies').getGuid('imdb://'+film["imdb_id"])
+            print("Found "+film["title"]+" on Plex server")
+        except:
+            print(film["title"]+" could not be found on Plex server")
+            continue
+
+        if film["imdb_id"] in plexImdbs:
+            print(film["title"]+" is already on Plex watchlist")
+        else:
+            plexAccount.addToWatchlist(plex_film)
+            print("Added "+film["title"]+" to Plex watchlist")
+
+        # Remove from plexImdbs so we are left with films to be removed from Plex watchlist.
+        try:
+            plexImdbs.remove(film["imdb_id"])
+        except ValueError as e:
+            print("Error removing "+film["title"]+" from Plex watchlist")
+
+    for imdbid in plexImdbs:
+        plex_film = server.library.section('Movies').getGuid('imdb://'+imdbid)
+        plexAccount.removeFromWatchlist(plex_film)
+        print("Removed "+plex_film+" from Plex watchlist")
+
+def main():
 
     # Add films to Plex watchlist
     plex_token = os.getenv('PLEX_TOKEN')
     plex_host = os.getenv('PLEX_HOST')
     if plex_token != '' and plex_host != '':
-        plex_watchlist_sync(watchlist,plex_host,plex_token)
+        plex_watchlist_sync(plex_host,plex_token)
     else:
         print("Missing PLEX_TOKEN and/or PLEX_HOST")
 
